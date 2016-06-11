b11c5ba7528986a24928eb8e450c79c0fcb5d6c1
ToruNiina <niina.toru.68u@gmail.com>
2016-06-11 14:56:06 +0900

    add sample

---
+++ sample/Sample.cpp
@@ -0,0 +1,71 @@
+#include "Sample.hpp"
+using namespace qahwa;
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0]
+                  << " <anchor distance> <anchor coef>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const double anchor_dist = std::stod(std::string(argv[1]));
+    const double anchor_coef = std::stod(std::string(argv[2]));
+    const std::string output_name("sample_dist_" + std::string(argv[1]) +
+                                  "_coef_" + std::string(argv[2]) + ".dcd");
+    std::cout << "output = " << output_name << std::endl;
+
+    const PerturbingPotential perturb(anchor_coef, anchor_dist);
+    const TruePotential potential;
+
+    std::random_device rnd;
+    const unsigned int seed = rnd();
+    std::cout << "seed = " << seed << std::endl;
+    RandomNumberGenerator rng(seed);
+
+    Metropolis metropolis(/*T = */ 300.0);
+    const double top = 10.0;
+    const double btm = 5.0;
+
+    // the state of the system
+    System system;
+    system.pos = anchor_dist;
+    system.ene = perturb.energy(system.pos) + potential.energy(system.pos);
+
+    DCDData data;
+    data.nset() = 10000;
+    data.istart() = 0;
+    data.nstep_save() = 1;
+    data.nstep() = 10000;
+    data.nunit() = 10000;
+    data.verCHARMM() = 24;
+    data.nparticle() = 2;
+    data.delta_t() = 0.1;
+    data.signeture() = "CORD";
+    data.push_header("========================== Qahwa sample dcd ");
+    data.traj().push_back(make_snapshot(system));
+
+    for(std::size_t tstep = 0; tstep < 10000; ++tstep)
+    {
+        while(true)
+        {
+            const double trial_pos = rng.uniform(btm, top);
+            const double trial_ene = perturb.energy(trial_pos) + 
+                                     potential.energy(trial_pos);
+            const double dE = trial_ene - system.ene;
+            if(metropolis.accept(dE, rng))
+            {
+                system.pos = trial_pos;
+                system.ene = trial_ene;
+                data.traj().push_back(make_snapshot(system));
+                break;
+            }
+        }
+    }
+    DCDWriter writer(output_name);
+    writer.data() = data;
+    writer.write();
+
+    return EXIT_SUCCESS;
+}
---
+++ sample/Sample.hpp
@@ -0,0 +1,120 @@
+#ifndef QAHWA_SAMPLE
+#define QAHWA_SAMPLE
+#include "DCDWriter.hpp"
+#include <random>
+#include <cstddef>
+
+namespace qahwa
+{
+
+using DCDData = coffeemill::DCDData;
+using DCDWriter = coffeemill::DCDWriter;
+using Vector3d = ax::Vector3d;
+using SnapShot = std::vector<Vector3d>;
+using Trajectory = std::vector<SnapShot>;
+
+namespace constants
+{
+static constexpr double      k_B     = 1.986231313e-3;// [kcal/mol/K]
+}
+
+
+class PerturbingPotential
+{
+  public:
+    PerturbingPotential(const double coef, const double anch_dist)
+        : coef_(coef), anch_(anch_dist)
+    {}
+
+    double energy(const double pos) const
+    {
+        return coef_ * (pos - anch_) * (pos - anch_);
+    }
+
+  private:
+
+    const double coef_;
+    const double anch_;
+};
+
+// to find (1/x potential, 1/x^2 force)
+class TruePotential
+{
+  public:
+
+    TruePotential(){}
+
+    double energy(const double pos) const
+    {
+        return q1 * q2 * ke / pos;
+    }
+
+  private:
+
+    const double ke = 1e0;
+    const double q1 = -1.0;
+    const double q2 = 1.0;
+};
+
+class RandomNumberGenerator
+{
+  public:
+    RandomNumberGenerator(unsigned int seed = 10)
+        : seed_(seed), mt_(seed)
+    {}
+
+    const double uniform(const double min, const double max)
+    {
+        return (std::uniform_real_distribution<double>(min, max))(this->mt_);
+    }
+
+    const unsigned int get_seed() const {return seed_;}
+
+  private:
+    const unsigned int seed_;
+    std::mt19937       mt_;
+};
+
+class Metropolis
+{
+  public:
+
+    Metropolis(const double temperature)
+    {
+        this->beta_ = 1.0 / (temperature * constants::k_B);
+    }
+
+    bool accept(const double dE, RandomNumberGenerator& rng)
+    {
+        if(dE <= 0.0)
+            return true;
+        else
+            return rng.uniform(0.0, 1.0) < std::exp(-1.0 * this->beta_ * dE);
+    }
+
+    double  beta() const {return beta_;}
+    double& beta()       {return beta_;}
+
+  private:
+    double beta_;
+
+};
+
+struct System
+{
+    double pos;
+    double ene;
+};
+
+SnapShot make_snapshot(const System& sys)
+{
+    SnapShot ss;
+    ss.push_back(Vector3d(0e0, 0e0, 0e0));
+    ss.push_back(Vector3d(sys.pos, 0e0, 0e0));
+    return ss;
+}
+
+
+}
+
+#endif /* QAHWA_SAMPLE */
