1daabddcacef2b8d74d9743124490da8dfdcda16
ToruNiina <niina.toru.68u@gmail.com>
2016-06-11 13:01:06 +0900

    add perturbing potential and reaction coord

---
+++ src/UserDefinitionFunction.cpp
@@ -0,0 +1,21 @@
+#include "UserDefinitionFunction.hpp"
+
+namespace qahwa
+{
+
+// this is for example. anchor.
+double PerturbingPotential::operator()(const SnapShot& snapshot) const
+{
+    return this->coefficient_ *
+           len_square(snapshot[this->index_] - this->anchored_position_);
+}
+
+// this is for example. distance from origin.
+template<>
+double ReactionCoordinate<double>::operator()(const SnapShot& snapshot) const
+{
+    return length(snapshot[1]);
+}
+
+
+}
---
+++ src/UserDefinitionFunction.hpp
@@ -0,0 +1,73 @@
+#ifndef QAHWA_USER_DEFINITION_FUNCTION
+#define QAHWA_USER_DEFINITION_FUNCTION
+#include "Definitions.hpp"
+
+namespace qahwa
+{
+
+/*!
+ *  @brief functor that returns perturbing potential.
+ *  This returns the perturbing potential (if you want to use qahwa to umbrella
+ *  sampling, this will be umbrella potential).
+ *  Normally this will be anchor potential. @n
+ *  @n
+ *  In the case that you add some abnormal potential to your simulation,
+ *  modify this class to be able to calculate your abnormal potential. @n
+ *  @n 
+ *  It must be guaranteed that the definition of operator() is kept.
+ *  operator() accepts SnapShot type and returns double type.
+ *
+ *  @sa SnapShot
+ */
+class PerturbingPotential
+{
+  public:
+    PerturbingPotential()  = default;
+
+    PerturbingPotential(const std::size_t index, const double coef, const Vector3d& pos)
+        : index_(index), coefficient_(coef), anchored_position_(pos)
+    {}
+
+    ~PerturbingPotential() = default;
+
+    double operator()(const SnapShot& snapshot) const;
+
+    double  coef() const {return coefficient_;}
+    double& coef()       {return coefficient_;}
+
+    std::size_t  particle_index() const {return index_;}
+    std::size_t& particle_index()       {return index_;}
+
+  private:
+
+    std::size_t  index_; //!< index of anchored particle :NOTE: 0-based index!
+    double       coefficient_; //!< coefficient of the potential
+    Vector3d     anchored_position_; //!< 3d vector the particle is anchored
+};
+
+
+/*!
+ *  @brief functor that returns Reaction Coordinate.
+ *  @tparam T_coord type of Reaction Coordinate. normally double.
+ *
+ *  This accepts ShapShot and returns Reaction Coordinate. You can output
+ *
+ */
+template<typename T_coord>
+class ReactionCoordinate
+{
+  public:
+    using coordinate_type = T_coord;
+
+  public:
+    ReactionCoordinate() = default;
+    ~ReactionCoordinate() = default;
+
+    coordinate_type operator()(const SnapShot& snapshot) const;
+};
+
+
+}
+
+
+#endif /* QAHWA_USER_DEFINITION_FUNCTION */
