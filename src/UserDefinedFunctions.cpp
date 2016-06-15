8ed9d43710d1e6d64f625805edd1f416e8d30f8e
ToruNiina <niina.toru.68u@gmail.com>
2016-06-15 18:36:41 +0900

    remove template

--- src/UserDefinedFunctions.cpp
+++ src/UserDefinedFunctions.cpp
@@ -6,14 +6,16 @@ namespace qahwa
 // this is for example. anchor.
 double PerturbingPotential::operator()(const SnapShot& snapshot) const
 {
-    return this->coefficient_ *
-           len_square(snapshot.at(this->index_) - this->anchored_position_);
+    const double dist = length(snapshot.at(1));
+    const double d_nat = length(this->anchored_position_);
+
+    return this->coefficient_ * (dist - d_nat) * (dist - d_nat);
 }
 
 // this is for example. distance from origin.
 double ReactionCoordinate::operator()(const SnapShot& snapshot) const
 {
-    return length(snapshot.at(0) - snapshot.at(1));
+    return length(snapshot.at(1));
 }
 
 
