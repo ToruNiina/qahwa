30fd823dfc69be0bd4a126ee869e72a0fcf8dbe4
ToruNiina <niina.toru.68u@gmail.com>
2016-06-15 18:38:39 +0900

    add make_pdf func

--- src/ProbabilityDensityFunction.cpp
+++ src/ProbabilityDensityFunction.cpp
@@ -3,17 +3,46 @@
 namespace qahwa
 {
 
-void standardize(ProbabilityDensityFunction& pdf)
+ProbabilityDensityFunction make_pdf(const Histogram<std::size_t>& hist)
+{
+    std::size_t sum = 0;
+    for(auto iter = hist.cbegin(); iter != hist.cend(); ++iter)
+        sum += *iter;
+
+    const double standardize_constant = static_cast<double>(sum);
+
+    ProbabilityDensityFunction retval(hist.bins(), hist.range_begin(), hist.range_end());
+
+    auto riter = retval.begin();
+    for(auto iter = hist.cbegin(); iter != hist.cend(); ++iter)
+    {
+        *riter = *iter / standardize_constant;
+        ++riter;
+    }
+    assert(riter == retval.end());
+
+    return retval;
+}
+
+ProbabilityDensityFunction standardize(const ProbabilityDensityFunction& pdf)
 {
     double sum = 0e0;
     for(auto iter = pdf.cbegin(); iter != pdf.cend(); ++iter)
         sum += *iter;
+
     const double standardize_constant = sum;
 
-    for(auto iter = pdf.begin(); iter != pdf.cend(); ++iter)
-        *iter /= standardize_constant;
+    ProbabilityDensityFunction retval(pdf.bins(), pdf.range_begin(), pdf.range_end());
 
-    return;
+    auto riter = retval.begin();
+    for(auto iter = pdf.cbegin(); iter != pdf.cend(); ++iter)
+    {
+        *riter = *iter / standardize_constant;
+        ++riter;
+    }
+    assert(riter == retval.end());
+
+    return retval;
 }
 
 }
--- src/ProbabilityDensityFunction.hpp
+++ src/ProbabilityDensityFunction.hpp
@@ -1,13 +1,20 @@
 #ifndef QAHWA_PROBABILITY_DENSITY_FUNCTION
 #define QAHWA_PROBABILITY_DENSITY_FUNCTION
 #include "Histogram.hpp"
+#include "HistogramMaker.hpp"
+#include "UserDefinedFunctions.hpp"
 
 namespace qahwa
 {
 
 using ProbabilityDensityFunction = Histogram<double>;
+using PotentialOfMeanForce       = Histogram<double>;
 
-void standardize(ProbabilityDensityFunction& pdf);
+ProbabilityDensityFunction
+make_pdf(const Histogram<std::size_t>& hist);
+
+ProbabilityDensityFunction
+standardize(const ProbabilityDensityFunction& pdf);
 
 }
 
